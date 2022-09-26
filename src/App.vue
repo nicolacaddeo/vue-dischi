5a826cfb1bac740b3e0dcb7d5b7f872a19b4d866
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-09-26 21:59:43 +0200

    completed HeaderComponent

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <div id="app">
-    
+    <header>
+      <HeaderComponent />
+    </header>
+
   </div>
 </template>
 
 <script>
+  import HeaderComponent from './components/HeaderComponent.vue';
 
-
-export default {
+  export default {
   name: 'App',
   components: {
+    HeaderComponent
   }
 }
 </script>
 
 <style lang="scss">
-#app {
+  @import './style/main.scss';
+  @import './style/global_variables.scss';
+  
+  #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
 </style>
-    
+  
+
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="header">
+    <img src="../assets/logo.svg" alt="logo spotify">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+}
+</script>
+
+<style scoped lang="scss">
+@import '../style/global_variables.scss';
+
+#header {
+    position: fixed;
+    width: 100%;
+    height: 7vh;
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    background-color: $component-color;
+    img {
+        width: 50px;
+    }
+}
+</style>
